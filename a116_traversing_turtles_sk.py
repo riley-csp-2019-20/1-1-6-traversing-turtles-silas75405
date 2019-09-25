6b728aca523dcaddd4317c181b1f038b0e80b3a1
silas75405 <55497951+silas75405@users.noreply.github.com>
2019-09-25 10:20:14 -0400

    Add files via upload
    version 1 (1.1.6)

---
+++ a116_traversing_turtles_sk.py
@@ -0,0 +1,38 @@
+
+#   Add code to make turtles move in a circle and change colors.
+import turtle as trtl
+
+# create an empty list of turtles
+my_turtles = []
+
+# use interesting shapes and colors
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+
+for s in turtle_shapes:
+  t = trtl.Turtle(shape=s)
+  new_color = turtle_colors.pop()
+  t.color(new_color)
+
+  my_turtles.append(t)
+
+
+#  starting point
+startx = 0
+starty = 0
+#  movement 
+count = 0
+for t in my_turtles:
+      t.penup()
+      t.goto(startx, starty)
+      t.pendown()
+      t.right(45*count)
+      t.forward(50)
+      count = count + 1
+   
+#  seperation
+      startx=t.xcor()
+      starty=t.ycor()
+
+wn = trtl.Screen()
+wn.mainloop()
